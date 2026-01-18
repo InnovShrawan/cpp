47c4b85c8ef80b02e5f255aa24f2c3cf943a116c
Shrawan Mandal <shrawanmandal314@gmail.com>
2026-01-19 00:35:27 +0530

    Add files via upload

---
+++ datatype_variable.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=12;
+//     int b=34;
+//     cout<<"the value of a is "<<a ;
+//     cout<<"\n the value of b is "<<b;
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int num1, num2;
+    cout<<"Enter the value of num 1:";
+    cin>>num1;
+    cout<<"Enter the value of the num 2: ";
+    cin>>num2;
+    cout<<"the value of num1 is :"<<num1 <<"\n the value of  num 2 is : "<<num2;
+    cout<<"\n The sum is :"<<num1+num2;
+    return 0;
+}
